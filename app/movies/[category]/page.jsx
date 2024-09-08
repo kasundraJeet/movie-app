55fc3fb182b483da6b86cf49ecec454a1d09aaaf
kasundraJeet <kasundrajeet29@gmail.com>
2024-09-08 17:09:00 +0530

    added movie box in movie category

--- app/movies/[category]/page.jsx
+++ app/movies/[category]/page.jsx
@@ -1,16 +1,20 @@
 import { ServerApiHandler } from "~/helper/ServerApiHandler";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+import { Pagination } from "@/components/custom";
+import MovieBox from "../components/MovieBox";
 
 export default async function CelebrityCategory({ params, searchParams }) {
   const { category } = params;
   let data;
+  let current_page;
+  let total_pages;
 
   try {
     const res = await ServerApiHandler(
       `/api/movies/${category}?page=${searchParams.page || 1}`
     );
     data = await res;
+    total_pages = data.total_pages;
+    current_page = data.page;
   } catch (error) {
     return <p>Failed to load celebrities. Please try again later.</p>;
   }
@@ -20,10 +24,23 @@ export default async function CelebrityCategory({ params, searchParams }) {
   }
 
   return (
-    <section className="container">
-      <SyntaxHighlighter language="json" style={atomDark}>
-        {JSON.stringify(data, null, 2)}
-      </SyntaxHighlighter>
+    <section>
+      <h1>{category.replace("-", " ").toUpperCase()}</h1>
+      <section className="container py-8 space-y-8">
+        <ul className="grid grid-cols-4 gap-4">
+          {data.results.map((movie) => (
+            <MovieBox movie={movie} key={movie.id} />
+          ))}
+        </ul>
+        <div className="flex items-center justify-center">
+          <Pagination
+            currentPage={current_page}
+            totalPage={total_pages}
+            query="page"
+            path={`/movies/${category}`}
+          />
+        </div>
+      </section>
     </section>
   );
 }
