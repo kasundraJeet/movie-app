04555ec5c033b0cd639f5b5dfa14fe5318cbe5d3
kasundraJeet <kasundrajeet29@gmail.com>
2024-09-11 23:41:05 +0530

    added new top loader

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,12 @@
 import "./app.css";
 import LayoutWrapper from "@/components/layout/LayoutWrapper";
+import NextTopLoader from 'nextjs-toploader';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+      <NextTopLoader />
         <LayoutWrapper>{children}</LayoutWrapper>
       </body>
     </html>
